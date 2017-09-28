3821aad9c7235dc9c69e3b9167e61d35fee477cd
ABHISHEK GOSWAMI ( Hiro ) <abhishek@ddproperty.com>
2017-09-28 12:26:25 +0700

    FIX #3

--- SubredditExchange.py
+++ SubredditExchange.py
@@ -2,46 +2,45 @@ from flask import Flask, request, render_template
 from uuid import uuid4
 import requests
 import requests.auth
+import sys
 import urllib
-
-app_redirect_uri='http://localhost/reddit_callback'
-app_user_agent='subreddit_exchange_testing by /u/karldreher'
+import config
 
 def user_agent():
-    'subreddit_exchange_testing by /u/karldreher'
+    return config.app_user_agent
 
 def base_headers():
     return {"User-Agent": user_agent()}
 
-
 def save_created_state(state):
     pass
+
 def is_valid_state(state):
     return True
 
-
 app = Flask(__name__)
+
 @app.route("/")
+def homepage():
+	text = '<a href="%s">Authenticate with reddit</a>'
+	return text % make_authorization_url()
 
 def make_authorization_url():
     # Generate a random string for the state parameter
     # Save it for use later to prevent xsrf attacks
     state = str(uuid4())
     save_created_state(state)
-    params = {"client_id": app_client_id,
+    params = {"client_id": config.app_client_id,
               "response_type": "code",
               "state": state,
-              "redirect_uri": app_redirect_uri,
+              "redirect_uri": config.app_redirect_uri,
               "duration": "temporary",
               "scope": "identity"}
-    url = "https://www.reddit.com/api/v1/authorize?" + urllib.parse.urlencode(params)
+    url = "https://www.reddit.com/api/v1/authorize?" + sys.modules['urllib3.packages.six.moves.urllib.parse'].urlencode(params)
     return url
-
 
 def subreddit_exchange_app():
     return render_template('index.html', url=make_authorization_url())
-
-
 
 @app.route("/reddit_callback")
 def reddit_callback():
@@ -54,13 +53,14 @@ def reddit_callback():
         abort(403)
     code = request.args.get('code')
     access_token = get_token(code)
-    return "Your reddit username is: %s" % get_username(access_token)
+    username = get_username(access_token)
+    return "Your reddit username is: %s" % username
 
 def get_token(code):
-    client_auth = requests.auth.HTTPBasicAuth(app_client_id, app_client_secret)
+    client_auth = requests.auth.HTTPBasicAuth(config.app_client_id, config.app_client_secret)
     post_data = {"grant_type": "authorization_code",
                  "code": code,
-                 "redirect_uri": app_redirect_uri}
+                 "redirect_uri": config.app_redirect_uri}
     headers = base_headers()
     response = requests.post("https://ssl.reddit.com/api/v1/access_token",
                              auth=client_auth,
@@ -68,20 +68,17 @@ def get_token(code):
                              data=post_data)
     token_json = response.json()
     return token_json["access_token"]
-    
-    
+
 def get_username(access_token):
     headers = base_headers()
-    headers.update({"Authorization": "bearer " + access_token})
+    authHeader = {
+        "Authorization": "Bearer " + access_token
+    }
+    headers.update(authHeader)
     response = requests.get("https://oauth.reddit.com/api/v1/me", headers=headers)
     me_json = response.json()
     return me_json['name']
 
 
-
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
-
-
-    
+    app.run(host='0.0.0.0', port=8000)
---
+++ config.py
@@ -0,0 +1,4 @@
+app_client_id = '';
+app_client_secret = '';
+app_redirect_uri='http://localhost:8000/reddit_callback'
+app_user_agent='subreddit_exchange_testing by /u/karldreher'
